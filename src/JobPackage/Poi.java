662bc41c89b9736b4c46a734282d1e8d4ed21fe9
SoftDrink <SamuelD@SamuelD-PC>
2014-02-05 09:26:26 +0100

    Update of POI class !

--- src/JobPackage/Poi.java
+++ src/JobPackage/Poi.java
@@ -16,6 +16,7 @@ public class Poi {
 	private String name;
 	private String label;
 	private String text;
+	private String link;
 	
 	public Poi(int id) {
 		poiMap = new MappingPoi();
@@ -24,9 +25,100 @@ public class Poi {
 	
 	private void poiBuilder(ResultSet result) {
 		try {
-			
+			result.next();
+			this.name = result.getString("POI_NAME");
+			this.coordX = result.getInt("POI_X");
+			this.coordY = result.getInt("POI_Y");
+			this.label = result.getString("POI_LABEL");
+			this.text = result.getString("POI_DESCRIPTION");
+			this.link = result.getString("POI_LINK");
 		} catch (Exception e) {
-			
+			System.out.println("--- ERROR CREATING POI ---");
+			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @return the coordX
+	 */
+	public int getCoordX() {
+		return coordX;
+	}
+
+	/**
+	 * @param coordX the coordX to set
+	 */
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+
+	/**
+	 * @return the coordY
+	 */
+	public int getCoordY() {
+		return coordY;
+	}
+
+	/**
+	 * @param coordY the coordY to set
+	 */
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * @return the link
+	 */
+	public String getLink() {
+		return link;
+	}
+
+	/**
+	 * @param link the link to set
+	 */
+	public void setLink(String link) {
+		this.link = link;
 	}
 }
